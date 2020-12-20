e638bd00cbf3d7074560e5b4676c17916b1243f1
PC2014101864\Administrator <tengyizu@talkweb.com.cn>
2020-12-21 00:52:29 +0800

    add

---
+++ README.txt
@@ -0,0 +1,9 @@
+﻿工程说明
+ 
+1.t-model 实体类工程
+统一管理实体类
+
+linux编译说明
+cd t-model/
+执行deploy.sh进行上传到maven私服上
+sh deploy.sh
---
+++ src/main/java/com/wadex/demo/domain/system/SysUser.java
@@ -0,0 +1,68 @@
+package com.wadex.demo.domain.system;
+
+import java.util.Date;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+
+import lombok.Data;
+
+/**
+ * 用户对象 sys_user
+ * 
+ * @author twwade
+ */
+@Data
+@TableName("sys_user")
+public class SysUser extends BaseEntity {
+    private static final long serialVersionUID = 1L;
+    
+    /** 用户ID */
+    private Long userId;
+    
+    /** 部门ID */
+    private Long deptId;
+    
+    /** 用户账号 */
+    private String userName;
+    
+    /** 用户昵称 */
+    private String nickName;
+    
+    /** 用户邮箱 */
+    private String email;
+    
+    /** 手机号码 */
+    private String phonenumber;
+    
+    /** 用户性别 */
+    private String sex;
+    
+    /** 用户头像 */
+    private String avatar;
+    
+    /** 密码 */
+    private String password;
+    
+    /** 盐加密 */
+    private String salt;
+    
+    /** 帐号状态（0正常 1停用） */
+    private String status;
+    
+    /** 删除标志（0代表存在 2代表删除） */
+    private String delFlag;
+    
+    /** 最后登录IP */
+    private String loginIp;
+    
+    /** 最后登录时间 */
+    private Date loginDate;
+    
+    /** 部门对象 */
+    
+    /** 角色组 */
+    private Long[] roleIds;
+    
+    /** 岗位组 */
+    private Long[] postIds;
+}
